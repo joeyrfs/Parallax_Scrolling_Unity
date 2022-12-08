f3f2f30ba6888dc3baa0d580e6c3aaf7999c3f0b
JoeyRFS <51177843+joeyrfs@users.noreply.github.com>
2022-12-08 11:21:13 -0300

    Finalizado

---
+++ Scripts/variaveis.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class variaveis : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // transform.Translate(new Vector3(0.1f, 0, 0));
+        // transform.Rotate(new Vector3(0,0,0.5f));
+        // transform.localScale += new Vector3(-0.1f, -0.1f, 0);
+    }
+}
